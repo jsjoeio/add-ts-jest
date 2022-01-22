ca5abb710f77482eb89701e005d74213d399e453
Joe <joe@coder.com>
2022-01-22 16:29:40 +0000

    wip

---
+++ __tests__/lib.test.ts
@@ -0,0 +1,29 @@
+import path from "path";
+import os from "os";
+import { promises as fs } from "fs";
+import { readPackageJson } from "../src/lib";
+
+describe("readPackageJson", () => {
+  let testPrefix = "readPackageJson";
+  let pathToPackageJson = "";
+  let tmpDirPath = path.join(os.tmpdir(), testPrefix);
+
+  const packageJsonData = {
+    name: "test-package-json",
+  };
+
+  beforeEach(async () => {
+    await fs.mkdir(tmpDirPath);
+    pathToPackageJson = `${tmpDirPath}/package.json`;
+    await fs.writeFile(pathToPackageJson, JSON.stringify(packageJsonData));
+  });
+
+  afterEach(async () => {
+    await fs.rm(tmpDirPath, { recursive: true, force: true });
+  });
+
+  it("should return the packageJson as an object", () => {
+    const packageJson = readPackageJson(pathToPackageJson);
+    expect(packageJson.name).toBe("test-package-json");
+  });
+});
---
+++ src/lib.ts
@@ -0,0 +1,6 @@
+import { IPackageJson } from "package-json-type";
+export function readPackageJson(path: string): IPackageJson {
+  // TODO@jsjoeio implement by reading packageJson from path
+  // https://attacomsian.com/blog/nodejs-read-write-json-files
+  return {};
+}

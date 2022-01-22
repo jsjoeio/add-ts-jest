7b5f7ee1e3551d4e5904fc32edde8b45d9b4c32c
Joe <joe@coder.com>
2022-01-22 19:38:26 +0000

    wip

--- __tests__/lib.test.ts
+++ __tests__/lib.test.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import os from "os";
 import { promises as fs } from "fs";
-import { readPackageJson } from "../src/lib";
+import { readPackageJson, checkForDependencies } from "../src/lib";
 
 describe("readPackageJson", () => {
   let testPrefix = "readPackageJson";
@@ -25,5 +25,67 @@ describe("readPackageJson", () => {
   it("should return the packageJson as an object", async () => {
     const packageJson = await readPackageJson(pathToPackageJson);
     expect(packageJson.name).toBe("test-package-json");
+  });
+});
+
+describe("checkForDependencies", () => {
+  describe("with no dependencies", () => {
+    let testPrefix = "checkForDependencies";
+    let pathToPackageJson = "";
+    let tmpDirPath = path.join(os.tmpdir(), testPrefix);
+
+    const packageJsonData = {
+      name: "test-package-json",
+    };
+
+    beforeEach(async () => {
+      await fs.mkdir(tmpDirPath);
+      pathToPackageJson = `${tmpDirPath}/package.json`;
+      await fs.writeFile(pathToPackageJson, JSON.stringify(packageJsonData));
+    });
+
+    afterEach(async () => {
+      await fs.rm(tmpDirPath, { recursive: true, force: true });
+    });
+    it("should return false and the missingDependencies", async () => {
+      const packageJson = await readPackageJson(pathToPackageJson);
+      const dependencies = packageJson.devDependencies;
+
+      const actual = checkForDependencies(dependencies);
+      expect(actual.hasDependencies).toBe(false);
+      expect(actual.missingDependencies).toHaveLength(4);
+    });
+  });
+  describe("with dependencies", () => {
+    let testPrefix = "checkForDependencies";
+    let pathToPackageJson = "";
+    let tmpDirPath = path.join(os.tmpdir(), testPrefix);
+
+    const packageJsonData = {
+      devDependencies: {
+        "@types/jest": "^27.4.0",
+        jest: "^27.4.7",
+        "ts-jest": "^27.1.3",
+        typescript: "^4.5.5",
+      },
+    };
+
+    beforeEach(async () => {
+      await fs.mkdir(tmpDirPath);
+      pathToPackageJson = `${tmpDirPath}/package.json`;
+      await fs.writeFile(pathToPackageJson, JSON.stringify(packageJsonData));
+    });
+
+    afterEach(async () => {
+      await fs.rm(tmpDirPath, { recursive: true, force: true });
+    });
+    it("should return object with dependency details", async () => {
+      const packageJson = await readPackageJson(pathToPackageJson);
+      const dependencies = packageJson.devDependencies;
+
+      const actual = checkForDependencies(dependencies);
+      expect(actual.hasDependencies).toBe(true);
+      expect(actual.missingDependencies).toHaveLength(0);
+    });
   });
 });
--- src/lib.ts
+++ src/lib.ts
@@ -1,5 +1,5 @@
 import { promises as fs } from "fs";
-import { IPackageJson } from "package-json-type";
+import { IDependencyMap, IPackageJson } from "package-json-type";
 
 export async function readPackageJson(path: string): Promise<IPackageJson> {
   try {
@@ -10,7 +10,61 @@ export async function readPackageJson(path: string): Promise<IPackageJson> {
     // https://attacomsian.com/blog/nodejs-read-write-json-files
     return dataAsObj;
   } catch (error) {
-      console.error("something went wrong reading the package json")
+    console.error("something went wrong reading the package json");
   }
   return {};
+}
+
+interface DependencyDetails {
+  /** Whether or not all the required dependencies are present */
+  hasDependencies: boolean;
+  /** An array of strings of missing dependencies (names) */
+  missingDependencies: string[];
+}
+
+const TS_JEST_REQUIRED_DEPENDENCIES = [
+  "typescript",
+  "jest",
+  "ts-jest",
+  "@types/jest",
+];
+
+export function checkForDependencies(
+  dependencies: IDependencyMap | undefined
+): DependencyDetails {
+  const missingDependencies: DependencyDetails["missingDependencies"] = [];
+
+  const dependencyDetails: DependencyDetails = {
+    missingDependencies,
+    hasDependencies: false,
+  };
+
+  if (dependencies) {
+    const dependencyKeys = Object.keys(dependencies);
+    // https://stackoverflow.com/a/53606357/3015595
+    // TODO@jsjoeio finish this. need to add a statement inside every
+    // if doesn't include, add to missingDependencies array
+
+    TS_JEST_REQUIRED_DEPENDENCIES.every((d) => dependencyKeys.includes(d));
+    dependencyKeys.forEach((dependency) => {
+      if (TS_JEST_REQUIRED_DEPENDENCIES.includes(dependency)) {
+      }
+      /*
+       lets say i have this ["jest", "typescript"] 
+        what we want to do is loop over that array and check
+        "does this array have these"
+
+
+        */
+      // Assume we're missing all of them
+      // then loop through
+      // if the one we have right now is included in required, then remove it from the array
+      // if it has it, continue
+      // we have these ["jest"]
+      // we loop through. if
+      // if not, add to array of msising
+    });
+  }
+
+  return dependencyDetails;
 }
